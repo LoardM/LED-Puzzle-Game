3301b3f05ed35aca971dc3792236ed38c358a074
LoardM <71391753+LoardM@users.noreply.github.com>
2020-11-16 13:23:25 +0100

    Update Joystick.cs

--- Joystick.cs
+++ Joystick.cs
@@ -1,15 +1,14 @@
-﻿using System;
+
 using Unosquare.RaspberryIO;
-using Unosquare.WiringPi;
 using Unosquare.RaspberryIO.Abstractions;
-using System.Threading;
+
 
 namespace Game
 {
     public class Joystick
     {
         private IGpioPin JS;
-        public Joystick(JoystickE direction, P1 pin)
+        public Joystick(JoystickE direction, P1 pin)        //Setzt die Charakterdaten, den Namen und welche Pin dazugehört.
         {
             Direction = direction;
             JS = Pi.Gpio[pin];
@@ -21,40 +20,11 @@ namespace Game
 
         public bool Read
         {
-            get { return JS.Read(); }
+            get { return JS.Read(); }  //liest den Joystick aus und gibt das dann zurück.
 
 
 
         }
-
-        
-        
-
-
-
-        //private IGpioPin JSP;
-        //private IGpioPin JSU;
-        //private IGpioPin JSD;
-        //private IGpioPin JSL;
-        //private IGpioPin JSR;
-
-        //public Joystick()
-        //{
-        //    JSP = Pi.Gpio[P1.Pin37];
-        //    JSU = Pi.Gpio[P1.Pin35];
-        //    JSD = Pi.Gpio[P1.Pin33];
-        //    JSL = Pi.Gpio[P1.Pin31];
-        //    JSR = Pi.Gpio[P1.Pin29];
-
-        //    JSP.PinMode = GpioPinDriveMode.Input;
-        //    JSU.PinMode = GpioPinDriveMode.Input;
-        //    JSD.PinMode = GpioPinDriveMode.Input;
-        //    JSL.PinMode = GpioPinDriveMode.Input;
-        //    JSR.PinMode = GpioPinDriveMode.Input;
-
-
-
-    
 
 
     }
